7c4d6c943bdc4d7a182045ab79980d72c65efa65
Hardikcode9 <jeehardik2@gmail.com>
2026-08-17 22:50:24 +0530

    Fix Webpack native module crashes and bypass ESLint warnings for Vercel

--- Project/Back/routes/geminiRoutes.js
+++
@@ -1,8 +0,0 @@
-const express = require("express");
-const router = express.Router();
-
-const { chatWithGemini } = require("../controller/geminiController");
-
-router.post("/chat", chatWithGemini);
-
-module.exports = router;
